bfc3d96edc14d3d49704774ef842580b1c80cb0d
khattab <khattab@khtb.de>
2024-03-17 23:15:14 +0100

    works with source exclude

---
+++ build/CMakeFiles/khattab.exe.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/workset/genCmake/project/src/file1.c" "D:/workset/genCmake/build/CMakeFiles/khattab.exe.dir/project/src/file1.c.obj"
+  "D:/workset/genCmake/project/src/main.c" "D:/workset/genCmake/build/CMakeFiles/khattab.exe.dir/project/src/main.c.obj"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "D:/workset/genCmake/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/khattab.exe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/khattab.exe.dir/project/src/file1.c.obj"
+  "CMakeFiles/khattab.exe.dir/project/src/main.c.obj"
+  "khattab.exe.exe"
+  "khattab.exe.exe.manifest"
+  "khattab.exe.pdb"
+  "libkhattab.exe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/khattab.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
